05e1b32f2dfe5d0804f01f5647f690d12bb14e77
Josrodjr <Josrodjr@gmail.com>
2020-10-12 03:32:57 -0600

    declaration and half complex if completed

--- templates/utils/asm_constants.py
+++ templates/utils/asm_constants.py
@@ -3,7 +3,26 @@ comparisons = {
     '<=': 'ble',
     '>': 'bgt',
     '>=': 'bge',
-    '==': 'beq'
+    '==': 'beq',
+    '!=': 'bne'
+}
+
+conditions = {
+    '==': 'eq',
+    '!=': 'ne',
+    '>': 'gt',
+    '<': 'lt',
+    '>=': 'ge',
+    '<=': 'le'
+}    
+
+contrary_conditions = {
+    'eq': 'ne',
+    'ne': 'eq',
+    'gt': 'le',
+    'le': 'gt',
+    'lt': 'ge',
+    'ge': 'lt'
 }
 
 if_options = {
@@ -11,6 +30,23 @@ if_options = {
     'only_if': 'cmp $reg1, $reg2\n$comp $t_true'
 }
 
+# requires: reg1, reg2, comp, temp, contr, t_false
+if_complex = {
+    'if_false': 'cmp $reg1, $reg2\nite $comp\nstr$comp r0, [$temp]\nb$contr $t_false\n'
+}
+
 while_options = {
     'while': 'cmp $reg1, $reg2\n$comp $t_true'
+}
+
+declaration_options = {
+    'char': '.balign 4\n$name: .asciz "$value"',
+    'int': '.balign 4\n$name: .word $value',
+    'boolean': '.balign 4\n$name: .word $value'
+}
+
+address_options = {
+    'char': 'address_of_$name: .word $name',
+    'int': 'address_of_$name: .word $name',
+    'boolean': 'address_of_$name: .word $name'
 }
